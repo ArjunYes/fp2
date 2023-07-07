5e153df925548bffb4a9950a8a8a47c5d6f5c849
ArjunYes <codingnerrd1998@gmail.com>
2023-07-07 10:26:43 +0100

    Updates to new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,51 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+    // {
+    //   path: 'package',
+    //   loadChildren: () => import('./package/package.module').then(module => module.PackageModule)
+    // },
+    // {
+    //   path: 'case',
+    //   loadChildren: () => import('./case/case.module').then(module => module.CaseModule)
+    // },
+    // {
+    //   path: 'qc',
+    //   loadChildren: () => import('./quality-check/quality-check.module').then(module => module.QualityCheckModule)
+    // },
+    // {
+    //   path: 'audit-requests',
+    //   loadChildren: () => import('./audit-requests/audit-requests.module').then(module => module.AuditRequestsModule)
+    // },
+    // {
+    //   path: 'closing-requests',
+    //   loadChildren: () => import('./closing-requests/closing-requests.module').then(module => module.ClosingRequestsModule)
+    // },
+    // {
+    //   path: '401', component: AccessDeniedComponent
+    // },
+    // {
+    //   path: '', redirectTo: 'package', pathMatch: 'full'
+    // }
+    ]
+  },
+  {
+    path: '**', redirectTo: '', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+
+
+
 export class AppRoutingModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { LandingComponent } from './landing/landing.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    LandingComponent
   ],
   imports: [
     CommonModule
