6b024f54b4920a2da4f8f5416f6555a295ff1c32
ArjunYes <codingnerrd1998@gmail.com>
2023-08-27 22:43:58 +0100

    Terms and coond comp updated

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
+import { SharedModule } from '../shared/shared.module'
+
 
 export const routes: Routes = [
  
@@ -27,6 +29,7 @@ export const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild(routes)
   ]
 })
