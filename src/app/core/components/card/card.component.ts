cdd941c279be1b64db962747718e0786afbf98b5
Sohaib Aumer <sohaibaumer@hotmail.com>
2023-09-07 02:05:16 +0500

    fix dashboard page, add payment and success modals.

--- src/app/core/components/card/card.component.ts
+++ src/app/core/components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input, ElementRef, Renderer2, HostListener } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +7,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  @Output() closeCard = new EventEmitter<void>();
+  @Output() payClicked = new EventEmitter<void>();
+  @Input() selectedAmount?: number;
+  private tabPressCounter = 0;
 
+  paymentForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder, private renderer: Renderer2, private el: ElementRef) {
+    this.paymentForm = this.formBuilder.group({
+      cardholderName: [''],
+      cardNumber: [''],
+      expiryDate: [''],
+      cvv: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    const element = document.getElementById('main-dashboard');
+    this.renderer.setStyle(element, 'display', 'none');
+
+    const cardElement = document.getElementById('cardTitle');
+    setTimeout(() => {
+      cardElement?.focus();
+    }, 0);
+  }
+
+  resetStyle() {
+    const element = document.getElementById('main-dashboard');
+    this.renderer.setStyle(element, 'display', 'block');
+    const dashboardTitle = document.getElementById('dashboardTitle');
+    dashboardTitle?.focus();
+  }
+
+
+  @HostListener('keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent): void {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+
+      this.tabPressCounter++;
+      if (this.tabPressCounter % 7 === 0) {
+        const cardElement = document.getElementById('cardTitle');
+        setTimeout(() => {
+          cardElement?.focus();
+        }, 0);
+      }
+    }
+  }
+
+  close() {
+    this.closeCard.emit();
+    this.resetStyle();
+  }
+
+  onPay() {
+    this.submitted = true;
+    this.payClicked.emit();
+  }
 }
--- src/app/core/components/card/success/success.component.ts
+++ src/app/core/components/card/success/success.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Renderer2, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-success',
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./success.component.css']
 })
 export class SuccessComponent {
+  @Output() closeModal = new EventEmitter<void>();
+  private tabPressCounter = 0;
+  constructor(private renderer: Renderer2, ) {}
 
+  ngOnInit(): void {
+    const successTitle = document.getElementById('successTitle');
+    setTimeout(() => {
+      successTitle?.focus();
+    }, 0);
+  }
+
+  onCloseModal() {
+    this.closeModal.emit();
+    this.resetStyle()
+  }
+
+  resetStyle() {
+    const element = document.getElementById('main-dashboard');
+    this.renderer.setStyle(element, 'display', 'block');
+    const dashboardTitle = document.getElementById('dashboardTitle');
+    dashboardTitle?.focus();
+  }
+
+  @HostListener('keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent): void {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+
+      this.tabPressCounter++;
+      if (this.tabPressCounter % 6 === 0) {
+        const modalElement = document.getElementById('modal');
+        setTimeout(() => {
+          modalElement?.focus();
+        }, 0);
+      }
+    }
+  }
 }
